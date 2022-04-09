aa89590b739aa5ff00e7bbeee9355e2fe3497466
Leandro <souzaleandro79@gmail.com>
2022-04-09 13:13:35 -0300

    Atividade cálculo área de um triângulo

---
+++ areaTriangulo.java
@@ -0,0 +1,20 @@
+
+/*
+    Criar uma aplicação que calcule a área de um triângulo.
+    Sabendo que a base receberá o valor de 15  e a altura o valor de 7,5.
+    
+    Fórmula para calcular o triângulo: area = base * altura / 2:
+*/
+
+
+public class areaTriangulo {
+    public static void main(String[] args) {
+        
+        double base = 15;
+        double altura = 7.5;
+        double area = (base * altura) / 2;
+
+        System.out.print(" O valor da área do triangulo é: " + area);
+    }
+    
+}
